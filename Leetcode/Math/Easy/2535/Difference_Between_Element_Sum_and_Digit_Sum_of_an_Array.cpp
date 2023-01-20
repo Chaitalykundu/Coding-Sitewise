c532d647f848d020b472a0bddf86696b7de7c4d6
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2023-01-20 15:04:15 +0530

    2535. Difference Between Element Sum and Digit Sum of an Array

--- Leetcode/Math/Easy/2535/Difference_Between_Element_Sum_and_Digit_Sum_of_an_Array.cpp
+++ Leetcode/Math/Easy/2535/Difference_Between_Element_Sum_and_Digit_Sum_of_an_Array.cpp
@@ -4,14 +4,10 @@ public:
         int n = nums.size();
         int elementSum=0, digitSum=0;
         
-        for(int i=0;i<n;i++){
-            elementSum+=nums[i];
-        }
         
         for(int i=0;i<n;i++){
-            if(nums[i]<=9)
-                digitSum+=nums[i];
-            else{
+            elementSum+=nums[i];
+            
                 int num=nums[i];
                 while(num>0){
                     int d=num%10;
@@ -19,7 +15,6 @@ public:
                     num/=10;
                 }
             }
-        }
         return abs(elementSum-digitSum);
     }
 };
