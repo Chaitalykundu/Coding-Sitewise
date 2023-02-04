1ba5cf1483252510d429f6dc1e609494f3fb5c2d
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2023-02-04 19:11:29 +0530

    Update Fibonacci_Number.cpp

--- Leetcode/Math/Easy/509/Fibonacci_Number.cpp
+++ Leetcode/Math/Easy/509/Fibonacci_Number.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int fib(int n) {
+         int a=0, b =1, c, i;
         
-        int a=0, b =1, c, i;
-        
-        if(n==0)
-            return 0;
-        else if(n==1)
-            return 1;
+        if(n== 0|| n==1)
+            return n;
         for(i=2;i<=n ;i++){
             c = a + b;
             a=b;
